21b9d4dcee4e9ee87e03edea56befe34f74849ac
pridestalkerr <danartenie158@gmail.com>
2024-04-13 16:42:42 +0300

    configs and setup

---
+++ prettier.config.mjs
@@ -0,0 +1,15 @@
+/**
+ * @type {import('prettier').Config & import("@ianvs/prettier-plugin-sort-imports").PluginConfig}
+ */
+const config = {
+  tabWidth: 2,
+  singleQuote: false,
+  trailingComma: "all",
+  useTabs: false,
+  bracketSpacing: true,
+  printWidth: 100,
+  semi: true,
+  endOfLine: "lf",
+};
+
+export default config;
